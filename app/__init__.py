46245ca23ff917ad98024b48fba1ef70d0f7d399
Chaitra122 <chaitranainoor@gmail.com>
2024-10-16 20:12:21 +0530

    Delete app directory

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-# Import routes
-from . import main
-
--- app/models/document_model.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-# Model representing the structure of a document
-class Document(BaseModel):
-    title: str
-    content: str
